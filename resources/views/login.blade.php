064e6bb7705eb3bcf42b462a270d8deb848a306a
yasin khadem <yasin.khadem77@gmail.com>
2021-02-22 19:48:03 +0330

    create header, footer, login form and create rtl direction and adding vazir font

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,29 @@
 @extends('master')
 @section('content')
-    <h1>login</h1>
-    <button class="btn btn-primary">
-        login
-    </button>
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-6 col-md-4  mx-auto">
+
+            <form dir="rtl">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">آدرس ایمیل</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="emailHelp" class="form-text text-muted">فقط ایمیل شخصی خود را استفاده کنید</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">رمز عبور</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input mr-0" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <div class="d-flex flex-row">
+                    <button type="submit" class="btn btn-primary ml-auto">Submit</button>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
